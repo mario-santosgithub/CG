8b75d2ab5f0f5cfef60761d04d9e45692cad3d1d
Mario Santos <marioguilherme@tecnico.ulisboa.pt>
2023-05-15 12:59:58 +0100

    stuff added

---
+++ workB/js/main-script.js
@@ -0,0 +1,153 @@
+//////////////////////
+/* GLOBAL VARIABLES */
+//////////////////////
+var camera, scene, renderer;
+var geometry, material, mesh;
+var ball;
+/////////////////////
+/* CREATE SCENE(S) */
+/////////////////////
+function createScene() {
+    'use strict';
+
+    scene = new THREE.Scene();
+
+
+    scene.add(new THREE.AxisHelper(10));
+
+    createBall(0, 0, 15);
+}
+
+function createBall(x, y, z) {
+    'use strict';
+
+    ball = new THREE.Object3D();
+    ball.userData = { jumping: true, step: 0 };
+
+    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+    geometry = new THREE.SphereGeometry(4, 10, 10);
+    mesh = new THREE.Mesh(geometry, material);
+
+    
+}
+
+//////////////////////
+/* CREATE CAMERA(S) */
+//////////////////////
+function createCamera() {
+    'use strict';
+    camera = new THREE.PerspectiveCamera(70,
+                                         window.innerWidth / window.innerHeight,
+                                         1,
+                                         1000);
+    camera.position.x = 50;
+    camera.position.y = 50;
+    camera.position.z = 50;
+    camera.lookAt(scene.position);
+}
+
+/////////////////////
+/* CREATE LIGHT(S) */
+/////////////////////
+
+////////////////////////
+/* CREATE OBJECT3D(S) */
+////////////////////////
+function createBall(x, y, z) {
+    'use strict';
+
+    ball = new THREE.Object3D();
+    ball.userData = { jumping: true, step: 0 };
+
+    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+    geometry = new THREE.SphereGeometry(4, 10, 10);
+    mesh = new THREE.Mesh(geometry, material);
+
+    ball.add(mesh);
+    ball.position.set(x, y, z);
+
+    scene.add(ball);
+}
+//////////////////////
+/* CHECK COLLISIONS */
+//////////////////////
+function checkCollisions(){
+    'use strict';
+
+}
+
+///////////////////////
+/* HANDLE COLLISIONS */
+///////////////////////
+function handleCollisions(){
+    'use strict';
+
+}
+
+////////////
+/* UPDATE */
+////////////
+function update(){
+    'use strict';
+
+}
+
+/////////////
+/* DISPLAY */
+/////////////
+function render() {
+    'use strict';
+    renderer.render(scene, camera);
+}
+
+////////////////////////////////
+/* INITIALIZE ANIMATION CYCLE */
+////////////////////////////////
+function init() {
+    'use strict';
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    createScene();
+    createCamera();
+
+    render();
+
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("resize", onResize);
+}
+
+/////////////////////
+/* ANIMATION CYCLE */
+/////////////////////
+function animate() {
+    'use strict';
+
+}
+
+////////////////////////////
+/* RESIZE WINDOW CALLBACK */
+////////////////////////////
+function onResize() { 
+    'use strict';
+
+}
+
+///////////////////////
+/* KEY DOWN CALLBACK */
+///////////////////////
+function onKeyDown(e) {
+    'use strict';
+
+}
+
+///////////////////////
+/* KEY UP CALLBACK */
+///////////////////////
+function onKeyUp(e){
+    'use strict';
+
+}
