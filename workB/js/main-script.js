a322c6506f6a3546a9b2c413fe2511faf346de07
Mario Santos <marioguilherme@tecnico.ulisboa.pt>
2023-05-16 23:32:11 +0100

    cameras implemented

--- workB/js/main-script.js
+++ workB/js/main-script.js
@@ -1,7 +1,9 @@
 //////////////////////
 /* GLOBAL VARIABLES */
 //////////////////////
-var camera, scene, renderer;
+var camera, camera1, camera2, camera3, camera4, camera5;
+var scene, renderer;
+var material, geometry, mesh;
 
 /////////////////////
 /* CREATE SCENE(S) */
@@ -11,23 +13,81 @@ function createScene() {
 
     scene = new THREE.Scene();
 
+    scene.add(new THREE.AxisHelper(20));
+    scene.background = new THREE.Color( 0xE7DAF9 );
 
-    scene.add(new THREE.AxisHelper(10));
+    createRobot();
+
 }
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
-function createCamera() {
+
+// Frontal
+function createCamera1() {
     'use strict';
-    camera = new THREE.PerspectiveCamera(70,
+    camera1 = new THREE.PerspectiveCamera(70,
                                          window.innerWidth / window.innerHeight,
                                          1,
                                          1000);
-    camera.position.x = 50;
-    camera.position.y = 50;
-    camera.position.z = 50;
-    camera.lookAt(scene.position);
+    camera1.position.x = 50;
+    camera1.position.y = 0;
+    camera1.position.z = 5;
+    camera1.lookAt(scene.position);
+}
+
+// Lateral
+function createCamera2() {
+    'use strict';
+    camera2 = new THREE.PerspectiveCamera(70,
+                                         window.innerWidth / window.innerHeight,
+                                         1,
+                                         1000);
+    camera2.position.x = 0;
+    camera2.position.y = 50;
+    camera2.position.z = 5;
+    camera2.lookAt(scene.position);
+}
+
+// Topo
+function createCamera3() {
+    'use strict';
+    camera3 = new THREE.PerspectiveCamera(70,
+                                         window.innerWidth / window.innerHeight,
+                                         1,
+                                         1000);
+    camera3.position.x = 0;
+    camera3.position.y = 0;
+    camera3.position.z = 50;
+    camera3.lookAt(scene.position);
+}
+
+// Ortogonal
+function createCamera4() {
+    'use strict';
+    camera4 = new THREE.PerspectiveCamera(70,
+                                         window.innerWidth / window.innerHeight,
+                                         1,
+                                         1000);
+    camera4.position.x = 50;
+    camera4.position.y = 50;
+    camera4.position.z = 50;
+    camera4.lookAt(scene.position);
+}
+
+// Perspetiva 
+function createCamera5() {
+    'use strict';
+    camera5 = new THREE.PerspectiveCamera(1000,
+                                         window.innerWidth / window.innerHeight,
+                                         1,
+                                         1000);
+    camera5.position.x = 0;
+    camera5.position.y = 0;
+    camera5.position.z = 100;
+    camera5.lookAt(scene.position);
+    camera5.rotateZ(Math.PI)
 }
 
 /////////////////////
@@ -37,6 +97,20 @@ function createCamera() {
 ////////////////////////
 /* CREATE OBJECT3D(S) */
 ////////////////////////
+
+function createRobot() {
+    'use strict'
+
+    var cube = new THREE.Object3D();
+    material = new THREE.MeshBasicMaterial ({color: 0xff3210, wireframe: false });
+    geometry = new THREE.BoxGeometry(10, 10, 10)
+    mesh = new THREE.Mesh(geometry, material);
+
+    cube.add(mesh);
+    cube.position.set(0, 0, 0);
+
+    scene.add(cube);
+}
 
 //////////////////////
 /* CHECK COLLISIONS */
@@ -82,7 +156,14 @@ function init() {
     document.body.appendChild(renderer.domElement);
 
     createScene();
-    createCamera();
+
+    createCamera1();
+    createCamera2();
+    createCamera3();
+    createCamera4();
+    createCamera5();
+
+    camera = camera4; // start with ortogonal
 
     render();
 
@@ -96,6 +177,10 @@ function init() {
 function animate() {
     'use strict';
 
+    render();
+
+    requestAnimationFrame(animate);
+
 }
 
 ////////////////////////////
@@ -103,6 +188,14 @@ function animate() {
 ////////////////////////////
 function onResize() { 
     'use strict';
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    if (window.innerHeight > 0 && window.innerWidth > 0) {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    }
+
 
 }
 
@@ -112,6 +205,24 @@ function onResize() {
 function onKeyDown(e) {
     'use strict';
 
+    switch (e.keyCode) {
+
+        case 49:
+            camera = camera1;
+            break;
+        case 50:
+            camera = camera2;
+            break;
+        case 51:
+            camera = camera3;
+            break;
+        case 52:
+            camera = camera4;
+            break;
+        case 53:
+            camera = camera5;
+            break;
+    }
 }
 
 ///////////////////////
