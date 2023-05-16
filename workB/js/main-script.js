c0108eb947a4f023780961580bbbc038933da1c6
Mario Santos <marioguilherme@tecnico.ulisboa.pt>
2023-05-16 22:14:51 +0100

    ball removed

--- workB/js/main-script.js
+++ workB/js/main-script.js
@@ -3,7 +3,7 @@
 //////////////////////
 var camera, scene, renderer;
 var geometry, material, mesh;
-var ball;
+
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
@@ -15,20 +15,6 @@ function createScene() {
 
     scene.add(new THREE.AxisHelper(10));
 
-    createBall(0, 0, 15);
-}
-
-function createBall(x, y, z) {
-    'use strict';
-
-    ball = new THREE.Object3D();
-    ball.userData = { jumping: true, step: 0 };
-
-    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-    geometry = new THREE.SphereGeometry(4, 10, 10);
-    mesh = new THREE.Mesh(geometry, material);
-
-    
 }
 
 //////////////////////
@@ -53,21 +39,7 @@ function createCamera() {
 ////////////////////////
 /* CREATE OBJECT3D(S) */
 ////////////////////////
-function createBall(x, y, z) {
-    'use strict';
 
-    ball = new THREE.Object3D();
-    ball.userData = { jumping: true, step: 0 };
-
-    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-    geometry = new THREE.SphereGeometry(4, 10, 10);
-    mesh = new THREE.Mesh(geometry, material);
-
-    ball.add(mesh);
-    ball.position.set(x, y, z);
-
-    scene.add(ball);
-}
 //////////////////////
 /* CHECK COLLISIONS */
 //////////////////////
